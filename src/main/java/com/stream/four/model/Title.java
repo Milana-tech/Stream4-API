56971823b953d27aaac6cf239eba887842580831
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2026-01-15 18:54:33 +0100

    add Title entity for movies and series

---
+++ src/main/java/com/stream/four/model/Title.java
@@ -0,0 +1,32 @@
+package com.stream.four.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "titles")
+
+public class Title {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    @Column(length = 2000)
+    private String description;
+
+    private int releaseYear;
+
+    @Enumerated(EnumType.STRING)
+    private TitleType type; // MOVIE or SERIES 
+    
+    private String genre; 
+    
+    private boolean deleted = false;
+}
