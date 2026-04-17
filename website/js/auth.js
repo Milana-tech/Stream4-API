cbca156f9175abc4fcd14c69ca4259fc0bc24699
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2026-04-17 20:25:07 +0200

    Update auth.js to use real API login

--- website/js/auth.js
+++ website/js/auth.js
@@ -1,10 +1,39 @@
+const API_BASE_URL = "http://localhost:8080/api";
+
 const loginForm = document.getElementById("loginForm");
 const registerBtn = document.getElementById("registerBtn");
 
-loginForm.addEventListener("submit", function(e) 
+loginForm.addEventListener("submit", async function(e)
 {
   e.preventDefault();
-  window.location.href = "profiles.html";
+
+  const email = document.getElementById("email".value);
+  const password = document.getElementById("password").value;
+
+  try {
+      const red = await fetch('${API_BASE_URL}/auth/login', {
+          method: "POST",
+          headers: {"Content-Type": "application.json"},
+          body: JSON.stringify({email, password})
+      });
+
+      if(red.ok)
+      {
+          alert("Invalid login");
+          return;
+      }
+
+      const data = await red.json();
+
+      //Save data
+      localStorage.setItem("token", data.token);
+
+      //Redirect to profiles page
+      window.location.href = "../profiles.html";
+  } catch (err) {
+      console.error(err);
+      alert("Server error");
+  }
 });
 
 registerBtn.addEventListener("click", function() 
